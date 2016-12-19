23d6c5d202442a40997faaa4b20aec644e44b295
g1nc <grimm.hellg@gmail.com>
2016-12-19 22:42:59 +0300

    Payment logic on dates change

--- app/models/salary.rb
+++ app/models/salary.rb
@@ -5,6 +5,8 @@ class Salary < ApplicationRecord
   FIRST_DATE = 5
   SECOND_DATE = 20
 
+  protected
+
   def set_dates
     if Salary.count.zero?
       if DateTime.now.day - FIRST_DATE < DateTime.now.day - SECOND_DATE
@@ -19,12 +21,19 @@ class Salary < ApplicationRecord
       self.starts_at = starts_at
       self.ends_at = ends_at
     else
-      last_payment = Salary.order(ends_at: :desc).first
-
+      last_payment = Salary.order(:created_at).last
       self.starts_at = DateTime.new(last_payment.ends_at.year, last_payment.ends_at.month, last_payment.ends_at.day + 1)
-      ends_at = DateTime.new(self.starts_at.year, self.starts_at.month, last_payment.starts_at.day - 1)
-      ends_at += 1.month
+
+      if last_payment.ends_at.day + 1 == FIRST_DATE
+        ends_at = DateTime.new(self.starts_at.year, self.starts_at.month, SECOND_DATE - 1)
+      else
+        ends_at = DateTime.new(self.starts_at.year, self.starts_at.month, FIRST_DATE - 1)
+        ends_at += 1.month
+      end
       self.ends_at = ends_at
     end
   end
+
+
+
 end
